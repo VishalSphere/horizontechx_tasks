1e552a267539248ef93916195ad1f13d325c49a1
vishal kumar singh <rajputvishal802157@gmail.com>
2026-07-02 20:24:28 +0530

    Add files via upload

---
+++ email_extractor.py
@@ -0,0 +1,16 @@
+import re
+
+# Read text file
+with open("sample.txt", "r") as file:
+    text = file.read()
+
+# Find email addresses
+emails = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}", text)
+
+# Save emails
+with open("emails.txt", "w") as file:
+    for email in emails:
+        file.write(email + "\n")
+
+print("Email extraction completed.")
+print("Total Emails Found:", len(emails))
